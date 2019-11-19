c4821dc31117cf526030fc48e03d33755352687d
David Simmer <david@simmerdesigns.com>
2019-11-19 09:31:25 -0800

    start with index.html

--- webpack/paths.js
+++ webpack/paths.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   root: path.resolve(__dirname, '../'),
   outputPath: path.resolve(__dirname, '../', 'build'),
-  templatePath: path.resolve(__dirname, '../', 'src/template.html'),
   entryPath: path.resolve(__dirname, '../', 'src/index.js'),
+  templatePath: path.resolve(__dirname, '../', 'src/index.html'),
   imagesFolder: 'images',
   fontsFolder: 'fonts',
   cssFolder: 'css',
