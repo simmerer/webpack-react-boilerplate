0e0cd752bbeaded7539f46742e74f5688b3c4edc
David Simmer <david@simmerdesigns.com>
2019-12-06 08:12:33 -0800

    stub in img dir

--- webpack/paths.js
+++ webpack/paths.js
@@ -5,7 +5,7 @@ module.exports = {
   outputPath: path.resolve(__dirname, '../', 'build'),
   entryPath: path.resolve(__dirname, '../', 'src/js/index.js'),
   templatePath: path.resolve(__dirname, '../', 'src/index.html'),
-  imagesFolder: 'images',
+  imagesFolder: 'img',
   fontsFolder: 'fonts',
   cssFolder: 'css',
   jsFolder: 'js',
